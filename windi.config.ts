01bfc2bd95ad1af6b4994ffc93d8e9febff747f8
cinob <cinob@foxmail.com>
2021-09-07 17:23:25 +0800

    🎉

--- windi.config.ts
+++ windi.config.ts
@@ -4,4 +4,14 @@ export default defineConfig({
   darkMode: 'class',
   // https://windicss.org/posts/v30.html#attributify-mode
   attributify: true,
+  theme: {
+    backgroundSize: {
+      auto: 'auto',
+      cover: 'cover',
+      contain: 'cover',
+      '50%': '50%',
+      '10%': '10%',
+      16: '4rem'
+    }
+  },
 })
